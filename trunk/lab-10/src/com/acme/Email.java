5faab577b5edf221c19fc348e45e02dd1bbe724d
tarasov.valeriy@gmail.com <tarasov.valeriy@gmail.com@455d54aa-c62c-fbc6-52d1-df4bc9bce61d>
2013-04-19 21:59:17 +0000

    branch->trunk

---
+++ trunk/lab-10/src/com/acme/Email.java
@@ -0,0 +1,9 @@
+package com.acme;
+
+public class Email {
+
+	public void setMessage(final String string) {
+
+	}
+
+}
---
+++ trunk/lab-10/src/com/acme/EmailService.java
@@ -0,0 +1,9 @@
+package com.acme;
+
+public class EmailService {
+
+	public void sendMail(final Email email) {
+		System.out.println("Email is sent");
+	}
+
+}
