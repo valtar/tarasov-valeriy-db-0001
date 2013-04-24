cc6516c4aa9d94370b19d1d4983f6dce4d7cbfa1
tarasov.valeriy@gmail.com <tarasov.valeriy@gmail.com@455d54aa-c62c-fbc6-52d1-df4bc9bce61d>
2013-04-24 07:57:35 +0000

    branch->head

---
+++ trunk/lab-15/src/com/acme/service/email/EmailService.java
@@ -0,0 +1,60 @@
+package com.acme.service.email;
+
+import com.acme.domain.email.Email;
+import com.acme.domain.email.Queue;
+import com.acme.domain.email.impl.AddToClosedQueueException;
+import com.acme.domain.email.impl.QueueImpl;
+
+public class EmailService {
+
+	private static EmailService instance = new EmailService();
+
+	private Queue queue = new QueueImpl();
+
+	private void sendEmail(Email email) {
+		System.out.println(email);
+	}
+
+	private boolean serviceClosed = false;
+
+	private EmailService() {
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					while (!serviceClosed) {
+						sendEmail(queue.getEmail());
+					}
+					while (!queue.isEmpty()) {
+						sendEmail(queue.getEmail());
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+
+		}.start();
+	}
+
+	public static EmailService getEmailService() {
+		return instance;
+	}
+
+	public void sendNotificationEmail(Email email) {
+		try {
+			queue.addEmail(email);
+		} catch (AddToClosedQueueException e) {
+			sendEmail(email);
+		}
+	}
+
+	public void close() {
+		serviceClosed = true;
+		queue.close();
+	}
+
+	public boolean isClosed() {
+		return serviceClosed;
+	}
+
+}
