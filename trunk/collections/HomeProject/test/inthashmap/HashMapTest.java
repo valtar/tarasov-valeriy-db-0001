ef9980eadbaa88965e7caf6eca50ee41ec6c5797
tarasov.valeriy@gmail.com <tarasov.valeriy@gmail.com@455d54aa-c62c-fbc6-52d1-df4bc9bce61d>
2013-05-24 14:02:12 +0000

    -mock lib

--- trunk/collections/HomeProject/test/inthashmap/HashMapTest.java
+++ trunk/collections/HomeProject/test/inthashmap/HashMapTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.*;
 
 public class HashMapTest {
 	private static final int VALUE = 2;
